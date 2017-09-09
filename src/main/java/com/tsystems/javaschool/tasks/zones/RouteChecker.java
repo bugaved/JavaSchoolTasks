8695e572f9f73ee2965641b8e5ec01ab4a5b2683
Viacheslav Rostov <bugaved@gmail.com>
2017-09-09 16:46:58 +0300

    pass tests

--- src/main/java/com/tsystems/javaschool/tasks/zones/RouteChecker.java
+++ src/main/java/com/tsystems/javaschool/tasks/zones/RouteChecker.java
@@ -4,47 +4,40 @@ import java.util.List;
 
 public class RouteChecker {
 
-
-    /**
-     * Checks whether required zones are connected with each other.
-     * By connected we mean that there is a path from any zone to any zone from the requested list.
-     * <p>
-     * Each zone from initial state may contain a list of it's neighbours. The link is defined as unidirectional,
-     * but can be used as bidirectional.
-     * For instance, if zone A is connected with B either:
-     * - A has link to B
-     * - OR B has a link to A
-     * - OR both of them have a link to each other
-     *
-     * @param zoneState        current list of all available zones
-     * @param requestedZoneIds zone IDs from request
-     * @return true of zones are connected, false otherwise
-     */
     public boolean checkRoute(List<Zone> zoneState, List<Integer> requestedZoneIds) {
         boolean cr = true;
+        boolean connected = false;
+        int conn = 0;
         if ((zoneState != null) && (requestedZoneIds != null)) {
-            int[] x = new int[requestedZoneIds.size()];
+            int[] x = new int[zoneState.size()];
             for (int k = 0; k < x.length; k++) {
-                x[k] = requestedZoneIds.get(k);
+                x[k] = zoneState.get(k).getId();
             }
-            for (int p = 0; p < zoneState.size(); p++)
+            for (int p = 0; p < zoneState.size(); p++) {
+                connected = false;
                 for (int q = 0; q < zoneState.size(); q++) {
-                    if (zoneState.get(q).getNeighbours().contains(zoneState.get(p).getId())) {
-                        for (int qq =0; qq<x.length; qq++) {
-                            if (x[qq] == zoneState.get(q).getId()) {
-                                x[qq] = zoneState.get(p).getId();
+                    if (zoneState.get(p).getNeighbours().contains(zoneState.get(q).getId())) {
+                        for (int qq = 0; qq < x.length; qq++) {
+                            if ((x[qq] == zoneState.get(p).getId()) && (connected == false)) {
+                                x[qq] = zoneState.get(q).getId();
+                                connected = true;
+                                conn = zoneState.get(q).getId();
+                            }
+                            if ((x[qq] == zoneState.get(q).getId()) && (connected == true)) {
+                                x[qq] = conn;
                             }
                         }
                     }
                 }
-            for (int pp = 1; pp < x.length; pp++) {
-                if (x[pp-1] != x[pp]) {
+            }
+            for (int xx = 1; xx < requestedZoneIds.size(); xx++) {
+                if (x[requestedZoneIds.get(xx - 1)-1] != x[requestedZoneIds.get(xx)-1]) {
                     cr = false;
                 }
             }
-
         }
         return cr;
     }
 
 }
+
