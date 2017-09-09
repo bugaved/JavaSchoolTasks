5d90287b905b94c21b5a144903aa040af5e033a6
Viacheslav Rostov <bugaved@gmail.com>
2017-09-09 19:11:53 +0300

    pass tests

--- src/main/java/com/tsystems/javaschool/tasks/zones/RouteChecker.java
+++ src/main/java/com/tsystems/javaschool/tasks/zones/RouteChecker.java
@@ -1,44 +1,77 @@
 package com.tsystems.javaschool.tasks.zones;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RouteChecker {
 
     public boolean checkRoute(List<Zone> zoneState, List<Integer> requestedZoneIds) {
-        boolean cr = true;
-        boolean connected ;
-        int conn = 0;
-        if ((zoneState != null) && (requestedZoneIds != null)) {
-            int[] x = new int[zoneState.size()];
-            for (int k = 0; k < x.length; k++) {
-                x[k] = zoneState.get(k).getId();
-            }
-            for (int p = 0; p < zoneState.size(); p++) {
-                connected = false;
-                for (int q = 0; q < zoneState.size(); q++) {
-                    if (zoneState.get(p).getNeighbours().contains(zoneState.get(q).getId())) {
-                        for (int qq = 0; qq < x.length; qq++) {
-                            if ((x[qq] == zoneState.get(p).getId()) && (connected == false)) {
-                                x[qq] = zoneState.get(q).getId();
-                                connected = true;
-                                conn = zoneState.get(q).getId();
-                            }
-                            if ((x[qq] == zoneState.get(q).getId()) && (connected == true)) {
-                                x[qq] = conn;
-                            }
-                        }
-                    }
-                }
-            }
 
-            for (int xx = 1; xx < requestedZoneIds.size(); xx++) {
-                if (x[requestedZoneIds.get(xx - 1)-1] != x[requestedZoneIds.get(xx)-1]) {
-                    cr = false;
-                }
-            }
-        }
-        return cr;
+        List<List<Integer>> tempList = getAllPermutations(requestedZoneIds);
+
+        return finalCheckRoute(zoneState, tempList);
+
     }
 
-}
+    private List<List<Integer>> getAllPermutations(List<Integer> requestedZoneIds) {
 
+        List<List<Integer>> permutations = new ArrayList<>();
+
+        permutations.add(new ArrayList<>());
+
+        for (int i = 0; i < requestedZoneIds.size(); i++) {
+
+            List<List<Integer>> current = new ArrayList<>();
+
+            for (List<Integer> permutation : permutations) {
+                for (int j = 0, n = permutation.size() + 1; j < n; j++) {
+                    List<Integer> temp = new ArrayList<Integer>(permutation);
+                    temp.add(j, requestedZoneIds.get(i));
+                    current.add(temp);
+                }
+            }
+            permutations = new ArrayList<>(current);
+        }
+
+        return permutations;
+
+    }
+
+    private Zone findZoneById(List<Zone> zoneState, Integer id) {
+
+        Zone resultZone = null;
+
+        for (Zone zone : zoneState) {
+            if (zone.getId() == id) {
+                resultZone = zone;
+            }
+        }
+        return resultZone;
+    }
+
+    private boolean finalCheckRoute(List<Zone> zoneState, List<List<Integer>> inputList) {
+
+        boolean connected = false;
+
+        for (List<Integer> variant : inputList) {
+
+            for (int i = 0; i < variant.size() - 1; i++) {
+
+                Zone source = findZoneById(zoneState, variant.get(i));
+                Zone target = findZoneById(zoneState, variant.get(i + 1));
+
+                if (source.getNeighbours().contains(target.getId()) || target.getNeighbours().contains(source.getId())) {
+                    connected = true;
+                } else {
+                    connected = false;
+                    break;
+                }
+            }
+
+            if (connected) {
+                break;
+            }
+        }
+        return connected;
+    }
+}
